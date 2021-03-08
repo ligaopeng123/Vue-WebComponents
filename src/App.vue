2041fec4c5d9b55a058975a17b9fa3c14f636161
ligaopeng123 <gaopeng@123>
2021-03-08 16:31:21 +0800

    合入人：pgli 合入时间：2021-3-8 合入问题：命名修改

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,49 @@
 <template>
-  <div id="app">
-    <user-card v-if="show" image="https://semantic-ui.com/images/avatar2/large/kristy.png"
-               name="User Name"
-               email="yourmail@some-email.com"
-    ></user-card>
+    <div id="app">
+        <user-card v-if="show" image="https://semantic-ui.com/images/avatar2/large/kristy.png"
+                   name="User Name"
+                   email="yourmail@some-email.com"
+        ></user-card>
 
-      <user-card
-              image="https://semantic-ui.com/images/avatar2/large/kristy.png"
-              name="test"
-              email="yourmail@some-email.com"
-      ></user-card>
+        <h265-video
+                image="https://semantic-ui.com/images/avatar2/large/kristy.png"
+                name="User Name"
+                email="yourmail@some-email.com"
+        ></h265-video>
 
-    <button @click="onclick">
-      测试
-    </button>
-  </div>
+        <button @click="onclick">
+            测试
+        </button>
+    </div>
 </template>
 
 <script>
-export default {
-  data: function() {
-    return {
-      show: true
-    }
-  },
-  name: 'App',
-  mounted() {
+    export default {
+        data: function () {
+            return {
+                show: true
+            }
+        },
+        name: 'App',
+        mounted() {
 
-  },
-  methods: {
-    // 测试web组件生命周期
-    onclick() {
-      this.show = !this.show;
+        },
+        methods: {
+            // 测试web组件生命周期
+            onclick() {
+                this.show = !this.show;
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
